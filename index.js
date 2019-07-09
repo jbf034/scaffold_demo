8fc89e0fb6bd1210ea1cb69e8a5fe4c566823fcb
jo <jo@agideo.com>
2019-07-09 22:56:59 +0800

    fix

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ program.version('1.0.0','-v, --version')
     }]).then((answers)=>{
        const spinner = ora('正在下载模板...');
        spinner.start();
-          download('jbf034/scaffold_demo',name,{clone: true},(err)=>{
+          download('jbf034/scaffold_demo/templates',name,{clone: true},(err)=>{
         if(err){
           spinner.fail();
           console.log(symbols.error, chalk.red(err))
